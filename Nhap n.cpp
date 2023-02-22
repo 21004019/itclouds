eddb9613db9d549b01005f07fd8efc90de73531e
21004019 <126036598+21004019@users.noreply.github.com>
2023-02-22 10:10:17 +0700

    Tinh tong tu 1 den n

--- Nhap n.cpp
+++ Nhap n.cpp
@@ -6,4 +6,9 @@ main()
 	cout<<"Nhap n: ";
 	cin>>n;
 	cout<<"So ban vua nhap la: "<<n;
+	//Tinh them tong tu 1 den n
+	int S = 0;
+	for (int i = 1; i<=n; i++)
+		S = S + i;
+	cout<<"Tong tu 1 den "<<n<<" = "<<S<<endl;
 }
